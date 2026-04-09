c32c4cb74c110163be1e5dfc05a9b40fcd587a33
edenziso <edenziso2003@gmail.com>
2026-04-09 08:34:15 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
         "content-type": "application/json"
       },
       body: JSON.stringify({
-        model: "claude-3-5-sonnet-20240620", // המודל הכי חזק 
+        model: "claude-3-haiku-20240307",
         max_tokens: 2000,
         system: systemPrompt,
         messages: [{ role: "user", content: message }]
